f60dbfe28968080b028b863275d377e324b68198
David-Fryd <david@fryd.com>
2023-05-02 14:47:28 -0400

    fix: Layout fpath

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
-import Layout from "~/layouts/layout";
+import Layout from "~/layouts/Layout";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
