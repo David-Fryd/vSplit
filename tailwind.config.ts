0c3ec5bef64f2ec284d59ec21adf09d7916a56b5
David-Fryd <david@fryd.com>
2023-05-02 13:31:45 -0400

    todo: custom fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,13 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        // TODO: Add local fonts after getting licensing/permission for them
+        // vedst: ["var(--font-edst)"],
+        // veram: ["var(--font-eram)"],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
