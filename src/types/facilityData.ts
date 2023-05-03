5d9dd0c0e74ab9ea63705d732a91b879af9aeedd
David-Fryd <david@fryd.com>
2023-05-03 01:06:08 -0400

    chore: geojson ingest logic

---
+++ src/types/facilityData.ts
@@ -0,0 +1,39 @@
+export type FacilityGeoJSON = {
+  type: string;
+  features: FacilityFeature[];
+};
+
+// FacilityFeature is A single "volume" (polygon associated w/ sectors & alts by the properties)
+type FacilityFeature = {
+  type: string; // "Feature" (Enforced in validation)
+  properties: {
+    /// SectorID -> AltitudeRange
+    [key: string]: string; // "23" : "0-999"
+    // NOTE: All properties are treated as if
+    // they are sector names/altitudes corresponding to the FacilityFeature
+    // NOTE: This is a result of encouraging people to build their GeoJSON
+    // files with geojson.io, which when you add a property adds it as a "key -> value"
+    // preventing us from using a more strucutred approach like properties: {{sector: string, altitudes: string}[]}
+  };
+  geometry: FacilityGeometry;
+  id?: number;
+};
+
+export type FacilityGeometry = {
+  coordinates: number[][][]; // [[[long, lat], [long, lat], ...], ...]]]
+  type: string; // "Polygon" (Enforced in validation)
+};
+
+// import type { LatLngExpression } from "leaflet";
+
+// export type GeoJSON = {
+//   type: string;
+//   features: {
+//     type: string;
+//     geometry: {
+//       type: string;
+//       coordinates: LatLngExpression[];
+//     };
+//     properties: unknown;
+//   }[];
+// };
---
+++ src/utils/parseFacilityGeoJSON.ts
@@ -0,0 +1,92 @@
+import type {
+  FacilityGeoJSON,
+  FacilityFeature,
+  FacilityGeometry,
+} from "~/types/facilityData";
+
+// TODO: Validation is pretty basic right now, but it's better than nothing
+// TODO: Add more granular failure
+
+function isFeatureCandidate(obj: unknown): obj is FacilityFeature {
+  return (
+    typeof obj === "object" &&
+    obj !== null &&
+    "type" in obj &&
+    "properties" in obj &&
+    "geometry" in obj
+  );
+}
+
+function isFacilityGeoJSONCandidate(obj: unknown): obj is FacilityGeoJSON {
+  return (
+    typeof obj === "object" &&
+    obj !== null &&
+    "type" in obj &&
+    "features" in obj
+  );
+}
+
+function isFacilityGeometryCandidate(obj: unknown): obj is FacilityGeometry {
+  return (
+    typeof obj === "object" &&
+    obj !== null &&
+    "type" in obj &&
+    "coordinates" in obj
+  );
+}
+
+/**
+ * Given the raw data from a facility's .geojson sector file, parse it into a FacilityGeoJSON object
+ * @param data The raw string data loaded from a facility's .geojson sector file
+ * @returns Parsed FacilityGeoJSON if the data is valid
+ * @throws Error if the data is invalid
+ */
+export function parseFacilityGeoJSON(data: unknown): FacilityGeoJSON {
+  if (!isFacilityGeoJSONCandidate(data)) {
+    throw new Error(
+      "The data is not a FacilityGeoJSON candidate (lacks type or features high-level field)"
+    );
+  }
+
+  if (data.type !== "FeatureCollection") {
+    throw new Error("The data is not of type 'FeatureCollection'");
+  }
+
+  if (!Array.isArray(data.features)) {
+    throw new Error("The features property is not an array");
+  }
+
+  data.features.forEach((feature: unknown, index: number) => {
+    if (!isFeatureCandidate(feature)) {
+      throw new Error(`Feature at index ${index} is not a valid candidate`);
+    }
+
+    if (feature.type !== "Feature") {
+      throw new Error(`Feature at index ${index} has an invalid type`);
+    }
+
+    if (feature.properties === undefined) {
+      throw new Error(`Feature at index ${index} has undefined properties`);
+    }
+
+    if (!isFacilityGeometryCandidate(feature.geometry)) {
+      throw new Error(
+        `Geometry of feature at index ${index} is not a valid candidate`
+      );
+    }
+
+    if (feature.geometry.type !== "Polygon") {
+      throw new Error(
+        `Geometry type of feature at index ${index} is not 'Polygon'`
+      );
+    }
+
+    if (!Array.isArray(feature.geometry.coordinates)) {
+      throw new Error(
+        `Coordinates of the geometry at feature index ${index} are not an array`
+      );
+    }
+  });
+
+  return data; // (as FacilityGeoJSON)
+}
