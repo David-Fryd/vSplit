b8fbad19bb935d92f14527791fba0092528a1f86
David-Fryd <david@fryd.com>
2023-05-03 14:46:24 -0400

    feat: Sidebar update

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -29,18 +29,54 @@ const ExternalLink = ({
   );
 };
 
+const MapNav = () => {
+  return (
+    <Link
+      className="m-0.5 flex h-20 items-center justify-center border-4 text-center text-lg"
+      href={"/"}
+    >
+      MAP
+    </Link>
+  );
+};
+
+const SettingsFlyout = () => {
+  return (
+    <p className="m-0.5 flex h-16 items-center justify-center border-4 text-center text-lg ">
+      DISPLAY SETTINGS
+    </p>
+  );
+};
+
+const QuickloadFlyout = () => {
+  return (
+    <p className="m-0.5 flex h-16 items-center justify-center border-4 text-center text-lg ">
+      QUICK LOAD
+    </p>
+  );
+};
+
 export const Sidebar = () => {
   return (
     <div className="flex h-screen w-28 flex-col bg-neutral-900 font-mono text-neutral-100">
-      <NavLink href="/">MAP</NavLink>
-      {/* <div className="p-4"></div>
-      <p className="text-md text-center text-green-500">AUTH REQ</p> */}
-      {/* <NavLink href="/">SECTOR CONFIG</NavLink> */}
-      <NavLink href="/sectorconfig">SECTOR CONFIG</NavLink>
-      <NavLink href="/firconfig">FIR CONFIG</NavLink>
-      <ExternalLink href="https://github.com/David-Fryd/easy-flash">
-        <p className="text-sm">BUGS & FEAT. REQS</p>
-      </ExternalLink>
+      <div className="h-full">
+        <MapNav />
+        <SettingsFlyout />
+        <div className="p-2"></div>
+        <p className="text-md text-center font-bold text-red-400">AUTH REQ</p>
+        <div className="m-1  border-2 border-red-400 px-0.5 py-0.5">
+          {/* <NavLink href="/">SECTOR CONFIG</NavLink> */}
+          <QuickloadFlyout />
+          <NavLink href="/sectorconfig">SECTOR CONFIG</NavLink>
+          <NavLink href="/firconfig">FIR CONFIG</NavLink>
+        </div>
+      </div>
+      <div className="flex-1">
+        <NavLink href="/about">ABOUT</NavLink>
+        <ExternalLink href="https://github.com/David-Fryd/easy-flash">
+          <p className="text-sm">BUGS & FEAT. REQS</p>
+        </ExternalLink>
+      </div>
     </div>
   );
 };
