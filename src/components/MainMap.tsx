f7e88164214671ffa27421a453bddff7df73f7cd
David-Fryd <david@fryd.com>
2023-05-03 13:06:07 -0400

    feat: multi-sector data load and display from .geojson

--- src/components/MainMap.tsx
+++ src/components/MainMap.tsx
@@ -23,116 +23,133 @@ import { bbox } from "@turf/turf";
 //   ],
 // };
 
+// Florida polygon
+const floridaPolygon: LatLngExpression[] = [
+  [31.707015, -87.890625],
+  [31.707015, -79.804688],
+  [24.527135, -79.804688],
+  [24.527135, -87.890625],
+];
+
+const coordinatesToLatLngExpression = (
+  coordinates: number[][][]
+): LatLngExpression[][] => {
+  return coordinates.map((ring) =>
+    ring.map((coord) => [coord[1], coord[0]] as LatLngExpression)
+  );
+};
+
+const renderPolygons = (features: FacilityFeature[]) => {
+  return features.map((feature: FacilityFeature, index) => {
+    const positions = coordinatesToLatLngExpression(
+      feature.geometry.coordinates
+    );
+    // Calculate the bounding box for the polygon
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const bounds = new LatLngBounds(positions);
+    // TODO, REMOVE AND FIX ABOVE BOUNDS CALC, perhaps manually define label lat/long in FIR CONFIG?
+    // Look more into smoothing out turf fxns into leaflet cords...
+
+    // const bboxBounds = bbox(positions);
+    // console.log("bboxBounds:", bboxBounds);
+    // Convert from bbox to LatLngBounds
+    // const bounds = new LatLngBounds(bboxBounds);
+
+    // const fetchAllFacilitiesGeoJSON = async () => {
+
+    // }
+
+    // Ultimately get the FeatureCollection as a typed GeoJSON JSON object
+
+    // Iterate through each facility GeoJSON: (every FIR has the name of its associated sector file hosted in the public directory)
+
+    // - For each Feature, examine properties
+    // - Draw polygon differently depending on whether one (or more) of the associated sector keys is in an active group
+
+    // Create a random hex color (i.e '#3388ff')
+    const randomHexColor = `#${Math.floor(Math.random() * 16777215).toString(
+      16
+    )}`;
+    return (
+      <div key={index} className="sector">
+        <Polygon
+          key={index}
+          pathOptions={{
+            color: randomHexColor,
+            // color: "blue",
+            weight: 1,
+            dashArray: "5, 5, 5, 2",
+          }}
+          positions={positions}
+        />
+        <SVGOverlay attributes={{ stroke: "red" }} bounds={bounds}>
+          <text x="50%" y="50%" /*stroke="white"*/ stroke={randomHexColor}>
+            {/* show every key value pair inside of feature.properties */}
+            {/* TODO, maybe some don't render on zoom level, etc... */}
+            {Object.entries(feature.properties).map(([key, value]) => (
+              <tspan className="font-mono" key={key} x="50%" dy="1.2em">
+                {key}: {value}
+              </tspan>
+            ))}
+          </text>
+        </SVGOverlay>
+      </div>
+    );
+  });
+};
+
 // const MainMap: React.FC<MainMapProps> = ({ sectorsWithVolumes }) => {
 const MainMap = () => {
-  // const sectorData = api.facilitydata.getAllSectorWithVolumes.useQuery();
+  const firData = api.facilitydata.getFIRsWithSectors.useQuery();
 
-  // Ultimately get the FeatureCollection as a typed GeoJSON JSON object
+  const [allFacilitiesGeoJSON, setAllFacilitiesGeoJSON] =
+    React.useState<FacilityGeoJSON[]>();
 
-  // Iterate through each facility GeoJSON: (every FIR has the name of its associated sector file hosted in the public directory)
+  const fetchAllFacilitiesGeoJSON = async () => {
+    console.log("fetchAllFacilitiesGeoJSON");
+    if (firData.data) {
+      try {
+        const fetchPromises = firData.data.map(async (fir) => {
+          const response = await fetch(`/facilityData/${fir.sectorFileName}`);
+          if (response.ok) {
+            const rawData = await response.text();
+            const geojsonData = parseFacilityGeoJSON(rawData);
+            return geojsonData;
+          } else {
+            console.error(
+              "Failed to fetch zmaSectors.geojson:",
+              response.statusText
+            );
+            return null;
+          }
+        });
 
-  // - For each Feature, examine properties
-  // - Draw polygon differently depending on whether one (or more) of the associated sector keys is in an active group
-
-  // Get the GeoJSON for the zmaSector under the /public folder
-  const [zmaSectorGeoJSON, setZmaSectorGeoJSON] =
-    React.useState<FacilityGeoJSON>();
-  const fetchZmaSectorGeoJSON = async () => {
-    try {
-      const response = await fetch("/facilityData/zmaSectors.geojson");
-      if (response.ok) {
-        const rawData = await response.text();
-        const geojsonData = parseFacilityGeoJSON(rawData);
-        setZmaSectorGeoJSON(geojsonData);
-      } else {
-        console.error(
-          "Failed to fetch zmaSectors.geojson:",
-          response.statusText
-        );
+        const fetchedData = await Promise.all(fetchPromises);
+        const typedFIRData = fetchedData.filter(
+          (data) => data !== null
+        ) as FacilityGeoJSON[];
+        console.log("typedFIRData:", typedFIRData);
+        setAllFacilitiesGeoJSON(typedFIRData);
+      } catch (error) {
+        console.error("Error fetching all facilities' .geojson data", error);
       }
-    } catch (error) {
-      console.error("Error fetching zmaSectors.geojson:", error);
+    } else {
+      console.log("no fir data");
     }
   };
 
   useEffect(() => {
-    fetchZmaSectorGeoJSON().catch((error) => {
-      console.error("Error fetching zmaSectors.geojson:", error);
+    console.log("useEffect");
+    fetchAllFacilitiesGeoJSON().catch((error) => {
+      console.error("Error fetching all facilities' .geojson data", error);
     });
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [firData.data]);
 
-  const coordinatesToLatLngExpression = (
-    coordinates: number[][][]
-  ): LatLngExpression[][] => {
-    return coordinates.map((ring) =>
-      ring.map((coord) => [coord[1], coord[0]] as LatLngExpression)
-    );
-  };
-
-  const renderPolygons = (features: FacilityFeature[]) => {
-    return features.map((feature: FacilityFeature, index) => {
-      const positions = coordinatesToLatLngExpression(
-        feature.geometry.coordinates
-      );
-
-      // Calculate the bounding box for the polygon
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      const bounds = new LatLngBounds(positions);
-      // TODO, REMOVE AND FIX ABOVE BOUNDS CALC, perhaps manually define label lat/long in FIR CONFIG?
-      // Look more into smoothing out turf fxns into leaflet cords...
-
-      // const bboxBounds = bbox(positions);
-      // console.log("bboxBounds:", bboxBounds);
-      // Convert from bbox to LatLngBounds
-      // const bounds = new LatLngBounds(bboxBounds);
-
-      // Create a random hex color (i.e '#3388ff')
-      const randomHexColor = `#${Math.floor(Math.random() * 16777215).toString(
-        16
-      )}`;
-      return (
-        <div key={index} className="sector">
-          <Polygon
-            key={index}
-            pathOptions={{
-              color: randomHexColor,
-              // color: "blue",
-              weight: 1,
-              dashArray: "5, 5, 5, 2",
-            }}
-            positions={positions}
-          />
-          <SVGOverlay attributes={{ stroke: "red" }} bounds={bounds}>
-            <text x="50%" y="50%" /*stroke="white"*/ stroke={randomHexColor}>
-              {/* show every key value pair inside of feature.properties */}
-              {/* TODO, maybe some don't render on zoom level, etc... */}
-              {Object.entries(feature.properties).map(([key, value]) => (
-                <tspan className="font-mono" key={key} x="50%" dy="1.2em">
-                  {key}: {value}
-                </tspan>
-              ))}
-            </text>
-          </SVGOverlay>
-        </div>
-      );
-    });
-  };
-
-  // Florida polygon
-  const floridaPolygon: LatLngExpression[] = [
-    [31.707015, -87.890625],
-    [31.707015, -79.804688],
-    [24.527135, -79.804688],
-    [24.527135, -87.890625],
-  ];
-
-  console.log("main map re-render");
+  // console.log("main map re-render");
   return (
     <div className="h-full w-full">
-      {/* <p className=" bg-neutral-500 text-white">
-        {zmaSectorGeoJSON && (JSON.stringify(zmaSectorGeoJSON) as any)}
-      </p> */}
       <MapContainer
         center={[35.8283, -98.5795]}
         zoom={4.5}
@@ -157,7 +174,10 @@ const MainMap = () => {
           }}
           positions={floridaPolygon}
         /> */}
-        {zmaSectorGeoJSON && renderPolygons(zmaSectorGeoJSON.features)}
+        {/* {zmaSectorGeoJSON && renderPolygons(zmaSectorGeoJSON.features)} */}
+        {allFacilitiesGeoJSON?.map((facilityGeoJSON) =>
+          renderPolygons(facilityGeoJSON.features)
+        )}
       </MapContainer>
     </div>
   );
--- src/server/api/routers/facilitydata.ts
+++ src/server/api/routers/facilitydata.ts
@@ -7,17 +7,39 @@ import {
 } from "~/server/api/trpc";
 
 export const facilityDataRouter = createTRPCRouter({
+  getFIRsWithSectors: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.fIR.findMany({
+      include: {
+        sectors: true,
+      },
+    });
+  }),
+
+  getAllFIR: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.fIR.findMany();
+  }),
+
+  getAllSectorFromFIR: publicProcedure
+    .input(z.object({ fir: z.string() }))
+    .query(({ input, ctx }) => {
+      return ctx.prisma.sector.findMany({
+        where: {
+          firName: input.fir,
+        },
+      });
+    }),
+
   getAllSector: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.sector.findMany();
   }),
 
-  getAllSectorWithVolumes: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.sector.findMany({
-      include: {
-        volumes: true,
-      },
-    });
-  }),
+  // getAllSectorWithVolumes: publicProcedure.query(({ ctx }) => {
+  //   return ctx.prisma.sector.findMany({
+  //     include: {
+  //       volumes: true,
+  //     },
+  //   });
+  // }),
 
   // hello: publicProcedure
   //   .input(z.object({ text: z.string() }))
