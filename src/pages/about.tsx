d524f642be22a424367072986242d975c27d7ff9
David-Fryd <david@fryd.com>
2023-05-11 17:09:18 -0400

    pages: about change

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,7 +6,13 @@ const About: NextPage = () => {
       <div className="flex h-screen w-1/2 flex-col items-center justify-center  p-2 font-mono text-neutral-50">
         <p className="text-center text-4xl font-bold text-green-500">ABOUT</p>
         <div className="p-4" />
-        <p className="text-center text-4xl">Easy Flash!</p>
+        <p className="text-center text-4xl font-bold">vSplit</p>
+        <div className="p-4" />
+        <p className="text-center text-2xl italic">
+          An open-source sector configuration mapping tool for VATSIM.
+        </p>
+        <div className="p-8" />
+        <p>Work In Progress</p>
       </div>
     </div>
   );
