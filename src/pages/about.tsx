439d160d5da9296dd8f91432aecd1143af61ab34
David-Fryd <david@fryd.com>
2023-05-25 15:43:01 -0400

    fix: escaped char build errors

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -26,15 +26,16 @@ const About: NextPage = () => {
           dimensional shapes (boundaries and altitude assignments) that have
           their own ID (and usually name/label). Oftentimes, these sectors are
           combined and distributed into groups of responsibility. This
-          "splitting" of groups is tedious, and the best way of doing it
-          currently is sitting down and manually drawing out what you want the
-          "split" to be. there are many different standard splits, and splits
-          that are specific to events, making it difficult for controllers to
-          know how to hand off aircraft and who is currently responsible for a
-          given piece of airspace. This tool allows controllers to see what the
-          split looks like at any given time by enabling ARTCCs to define their
-          sectors, and for authenticated/authorized controllers to shuffle
-          around the groupings of sectors.
+          &quot;splitting&quot; of groups is tedious, and the best way of doing
+          it currently is sitting down and manually drawing out what you want
+          the &quot;split&quot; to be. there are many different standard splits,
+          and splits that are specific to events, making it difficult for
+          controllers to know how to hand off aircraft and who is currently
+          responsible for a given piece of airspace. This tool allows
+          controllers to see what the split looks like at any given time by
+          enabling ARTCCs to define their sectors, and for
+          authenticated/authorized controllers to shuffle around the groupings
+          of sectors.
         </p>
       </div>
     </div>
