df8bc09f8b37cae813a76a7d0a9544c0cb8bf7c0
David-Fryd <david@fryd.com>
2023-05-25 15:40:49 -0400

    feat: about page basic description

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const About: NextPage = () => {
   return (
     <div className="flex h-screen w-full items-center justify-center border-2 bg-neutral-900">
-      <div className="flex h-screen w-1/2 flex-col items-center justify-center  p-2 font-mono text-neutral-50">
+      <div className="flex h-screen w-1/2 flex-col items-center justify-center  p-2  text-neutral-50">
         {/* <p className="text-center text-4xl font-bold text-green-500">ABOUT</p> */}
         <div className="p-4" />
         <div className="flex items-center gap-x-2">
@@ -20,8 +20,22 @@ const About: NextPage = () => {
         <p className="text-center text-2xl italic">
           An open-source sector configuration mapping tool for VATSIM.
         </p>
-        <div className="p-8" />
-        <p>Work In Progress</p>
+        <div className="p-4" />
+        <p className="text-justify text-lg">
+          ARTCCs define how to split up their airspace into sectors, which are 3
+          dimensional shapes (boundaries and altitude assignments) that have
+          their own ID (and usually name/label). Oftentimes, these sectors are
+          combined and distributed into groups of responsibility. This
+          "splitting" of groups is tedious, and the best way of doing it
+          currently is sitting down and manually drawing out what you want the
+          "split" to be. there are many different standard splits, and splits
+          that are specific to events, making it difficult for controllers to
+          know how to hand off aircraft and who is currently responsible for a
+          given piece of airspace. This tool allows controllers to see what the
+          split looks like at any given time by enabling ARTCCs to define their
+          sectors, and for authenticated/authorized controllers to shuffle
+          around the groupings of sectors.
+        </p>
       </div>
     </div>
   );
